0a8fb178033ba89931b2ba8d58c027a5808325e6
volonda <Volonda@mail.ru>
2017-10-27 10:20:32 +0300

    first

---
+++ src/main/java/finder/FileReader.java
@@ -0,0 +1,15 @@
+package finder;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileReader {
+    private int lines = 100;
+
+    public List<String> readPage(File file, int num) {
+        List lines = new ArrayList<>();
+
+        return lines;
+    }
+}
